6a06a85866083828080acba4339401dff02cfa34
Brinaldo Vorfi <brinaldo2002@gmail.com>
2025-08-07 12:37:22 +0200

    End of Section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,12 @@ namespace API.Entities
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public required string DisplayName { get; set; }
         public required string Email { get; set; }
+        public string? ImageUrl { get; set; }
         public required byte[] PasswordHash { get; set; }
         public required byte[] PasswordSalt { get; set; }
+
+        //Nav Property
+        
+        public Member Member { get; set; } = null!;
     }
 }
