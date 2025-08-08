a046522c65821d9d04d416cc30701bf09d6dae48
Brinaldo Vorfi <brinaldo2002@gmail.com>
2025-08-08 16:37:20 +0200

    End of section 9

--- API/Extensions/AppUserExtensions.cs
+++ API/Extensions/AppUserExtensions.cs
@@ -17,6 +17,7 @@ namespace API.Extensions
             Id = user.Id,
             DisplayName = user.DisplayName,
             Email = user.Email,
+            ImageUrl = user.ImageUrl,
             Token = tokenService.CreateToken(user)
             
         };
--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -3,7 +3,7 @@ export type User = {
     displayName:string;
     email:string;
     token?:string;
-    imageurl?:string;
+    imageUrl?:string;
 }
 
 export type LoginCreds = {
