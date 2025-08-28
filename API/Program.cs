d93b3713246f9f6b0dce90e1d8e22a4810aa0b6d
Brinaldo Vorfi <brinaldo2002@gmail.com>
2025-08-28 13:41:30 +0200

    deployment error fixed

--- API/Program.cs
+++ API/Program.cs
@@ -86,7 +86,7 @@ app.UseCors(x => x
      .AllowAnyHeader()
     .AllowAnyMethod()
     .AllowCredentials()
-    .WithOrigins("http://localhost:4200", "https://localhost:4200","https://localhost:5001" ));
+    .WithOrigins("http://localhost:4200", "https://localhost:4200","https://localhost:5001","https://da-2025-vorfi.azurewebsites.net" ));
 
 app.UseAuthentication();
 app.UseAuthorization();
